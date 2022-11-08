16878456bd0fd49727a5fe5b980faab35c8a43c7
Dan Allen <dan@opendevise.com>
2022-11-08 03:49:02 -0700

    configure Antora and workflow to support partial build

--- .github/actions/publish-docs.sh
+++ .github/actions/publish-docs.sh
@@ -16,7 +16,16 @@ fi
   install -m 600 -D /dev/null "$SSH_PRIVATE_KEY_PATH"
   echo "$SSH_PRIVATE_KEY" > "$SSH_PRIVATE_KEY_PATH"
   echo "$SSH_KNOWN_HOST" > ~/.ssh/known_hosts
-  rsync --delete -avze "ssh -i $SSH_PRIVATE_KEY_PATH" build/site/ "$HOST:$HOST_PATH"
+  if [ -n "$BUILD_REFNAME" ]; then
+    VERSION_DIR=`find build/site -maxdepth 1 -type d \( -name '[0-9]*' \) -printf '%f\t' | cut -f1`
+    if [ -n "$VERSION_DIR" ]; then
+      rsync --include '/sitemap*.xml' --include /site-manifest.json --include "/$VERSION_DIR/" --include "/$VERSION_DIR/**" --exclude '/**' --delete -avze "ssh -i $SSH_PRIVATE_KEY_PATH" build/site/ "$HOST:$HOST_PATH"
+    else
+      rsync --exclude '/_/' --exclude /404.html --exclude /.htaccess --exclude '/[1-9].*' --exclude '/[1-9][0-9].*' --delete -avze "ssh -i $SSH_PRIVATE_KEY_PATH" build/site/ "$HOST:$HOST_PATH"
+    fi
+  else
+    rsync --delete -avze "ssh -i $SSH_PRIVATE_KEY_PATH" build/site/ "$HOST:$HOST_PATH"
+  fi
 )
 exit_code=$?
 
