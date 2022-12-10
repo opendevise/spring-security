6fbb068135bf6ff2c3edb9148b0ac627f6541558
Dan Allen <dan@opendevise.com>
2022-12-10 02:00:45 -0700

    debug cache invalidation

--- .github/actions/publish-docs.sh
+++ .github/actions/publish-docs.sh
@@ -17,19 +17,19 @@ fi
 (
   set -e
   set -f
-  install -m 600 -D /dev/null "$SSH_PRIVATE_KEY_PATH"
-  echo "$SSH_PRIVATE_KEY" > "$SSH_PRIVATE_KEY_PATH"
-  echo "$SSH_KNOWN_HOST" > ~/.ssh/known_hosts
-  RSYNC_OPTS='-avz --delete'
-  if [ -f .full-build ]; then
-    unlink .full-build
-    BUILD_REFNAME=
-  fi
-  if [ -n "$BUILD_REFNAME" ]; then
-    RSYNC_OPTS="-c $RSYNC_OPTS$(find $FROM -mindepth 1 -maxdepth 1 \! -name 404.html \! -name '.*' -type f -printf ' --include /%P')"
-    RSYNC_OPTS="$RSYNC_OPTS$(find $FROM -mindepth 1 -maxdepth 1 -type d \! -name _ -printf ' --include /%P --include /%P/**') --exclude **"
-  fi
-  rsync $RSYNC_OPTS -e "ssh -i $SSH_PRIVATE_KEY_PATH" $FROM/ "$HOST:$HOST_PATH"
+  #install -m 600 -D /dev/null "$SSH_PRIVATE_KEY_PATH"
+  #echo "$SSH_PRIVATE_KEY" > "$SSH_PRIVATE_KEY_PATH"
+  #echo "$SSH_KNOWN_HOST" > ~/.ssh/known_hosts
+  #RSYNC_OPTS='-avz --delete'
+  #if [ -f .full-build ]; then
+  #  unlink .full-build
+  #  BUILD_REFNAME=
+  #fi
+  #if [ -n "$BUILD_REFNAME" ]; then
+  #  RSYNC_OPTS="-c $RSYNC_OPTS$(find $FROM -mindepth 1 -maxdepth 1 \! -name 404.html \! -name '.*' -type f -printf ' --include /%P')"
+  #  RSYNC_OPTS="$RSYNC_OPTS$(find $FROM -mindepth 1 -maxdepth 1 -type d \! -name _ -printf ' --include /%P --include /%P/**') --exclude **"
+  #fi
+  #rsync $RSYNC_OPTS -e "ssh -i $SSH_PRIVATE_KEY_PATH" $FROM/ "$HOST:$HOST_PATH"
   if [ -z "$BUILD_REFNAME" ]; then
     curl -X POST "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/purge_cache" \
       -H "Content-Type:application/json" -H "Authorization: Bearer $CLOUDFLARE_CACHE_TOKEN" \
@@ -38,6 +38,6 @@ fi
 )
 exit_code=$?
 
-rm -f "$SSH_PRIVATE_KEY_PATH"
+#rm -f "$SSH_PRIVATE_KEY_PATH"
 
 exit $exit_code
